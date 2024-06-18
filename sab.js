c21e87de8db2feb103afac0e5c44144f29f9b8ca
Mr-Adilet <adi944488@gmail.com>
2024-06-18 23:26:50 +0600

    Add files via upload

---
+++ sab.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const searchInput = document.getElementById('search-input');
+    const searchForm = document.getElementById('search-form');
+    const suggestions = document.getElementById('suggestions');
+
+    const fighters = [
+        { name: 'Брэндон Морено', url: 'Брэндон Морено.html' },
+        { name: 'Александр Пантожа', url: 'Карточки бойца/alexandre_pantoja.html' },
+        { name: 'Шон О\'Мэлли', url: 'Карточки бойца/sean_omalley.html' },
+        { name: 'Илия Топурия', url: 'Карточки бойца/ilia_topuria.html' },
+        { name: 'Дрикус Дю Плесси', url: 'Карточки бойца/dricus_du_plessis.html' },
+        { name: 'Алекс Перейра', url: 'Карточки бойца/alex_pereira.html' },
+        { name: 'Ислам Махачев', url: 'Карточки бойца/islam_makhachev.html' },
+        { name: 'Леон Эдвардс', url: 'Карточки бойца/leon_edwards.html' },
+        { name: 'Джон Джонс', url: 'Карточки бойца/jon_jones.html' },
+        { name: 'Брэндон Ройвал', url: 'Брэндон Ройвал.html' }
+    ];
+
+    searchInput.addEventListener('input', () => {
+        const query = searchInput.value.toLowerCase();
+        suggestions.innerHTML = '';
+        if (query.length > 0) {
+            const filteredFighters = fighters.filter(fighter => fighter.name.toLowerCase().includes(query));
+            filteredFighters.forEach(fighter => {
+                const suggestion = document.createElement('div');
+                suggestion.textContent = fighter.name;
+                suggestion.addEventListener('click', () => {
+                    window.location.href = fighter.url;
+                });
+                suggestions.appendChild(suggestion);
+            });
+        }
+    });
+
+    searchForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const query = searchInput.value.toLowerCase();
+        const fighter = fighters.find(fighter => fighter.name.toLowerCase() === query);
+        if (fighter) {
+            window.location.href = fighter.url;
+        } else {
+            alert('Боец не найден!');
+        }
+    });
+});
